ac2cfa2407564cdf523e4491e8a9139228db2321
Gustavo Rosa <gth.rosa@uol.com.br>
2020-07-31 10:21:29 -0300

    Removing old data and adding a text loading utility.

---
+++ examples/utils/load_text.py
@@ -0,0 +1,7 @@
+import textfier.utils.loader as l
+
+# Loads an input .txt file
+text = l.load_txt('data/sample.txt')
+
+# Printing loaded text
+print(text)
---
+++ textfier/utils/loader.py
@@ -0,0 +1,40 @@
+"""Data-loading utilities.
+"""
+
+import textfier.utils.logging as l
+
+logger = l.get_logger(__name__)
+
+
+def load_txt(file_name):
+    """Loads a .txt file.
+
+    Args:
+        file_name (str): The file name to be loaded.
+
+    Returns:
+        A string with the loaded text.
+
+    """
+
+    logger.debug('Loading %s ...', file_name)
+
+    # Tries to load the file
+    try:
+        # Opens the .txt file
+        file = open(file_name, 'rb')
+
+        # Reads the text
+        text = file.read().decode(encoding='utf-8')
+
+        return text
+
+    # If file can not be loaded
+    except FileNotFoundError:
+        # Creates an error
+        e = f'File not found: {file_name}.'
+
+        # Logs the error
+        logger.error(e)
+
+        raise
